712cfa79b208875f6cd615f3d482808859ec0a74
tkrogers3 <tkrogers3@gmail.com>
2020-04-15 19:17:10 -0400

    Relationship and Bio working

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "heroes";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+require "connection.php";
+require "header.php";
+
+echo '<div class = "text-center"> <h1> Super Friends! </h1></div>';
+
+$sql = "SELECT * FROM heroes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $output = "";
+    echo '<div class="container bg-secondary">';
+    echo '<div class="row">';
+    while ($row = $result->fetch_assoc()) {
+        $hero = "hero.php?id=" . $row["id"];
+        $output .= '
+        <div class="col-4" >
+            <div class="card mt-2 mb-3" style="height:60vh">
+                <img class= "card-img-top" alt="hero image" src= "./Images/hero'. $row["id"] .'.jpg" />
+                    <div class="card-body bg-info">
+                        <h5 class="card-title text-white">' . $row["name"] . '</h5>
+                        <p class="card-text">' . $row["about_me"] . '</p>
+                        <a href=' . $hero . ' class="btn btn-secondary">Profile</a>
+                    </div>
+                    </div>
+                </div>';
+    }
+
+    echo $output;
+    echo '</div>';
+} else {
+    echo "0 results";
 }
 
-
-
-mysqli_close($conn);
+$conn->close();
+require "footer.php";
 ?>
+
